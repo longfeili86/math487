f8da2ce740cea1504156e5542f1ee827ba12c258
Longfei Li <longfei.li86@gmail.com>
2019-09-06 15:31:01 -0500

    hw1 added

---
+++ Homework/HW1/mycbrt.py
@@ -0,0 +1,13 @@
+# README:
+# This is the stater code for your homework 1 #
+# complete the definition of the function so that it can compute cube root (cbrt)
+# of a number. Do not change the function interface
+
+def cbrt(S,x0=1.,tol=1e-5,debug=False):
+    #Input:
+    #       S: the number we want to compute its cube root
+    #      x0: initial guess. We have provided a default value, so it's optional
+    #      tol: tolerance. Default values is provided as tol=1e-5
+    #debug: a True/False flag to indicate if to turn on the debugging mode
+
+    
---
+++ Homework/HW1/nthrt.py
@@ -0,0 +1,18 @@
+# README
+# This is the stater code for your homework 1 #
+# complete the definition of the function so that it can compute nth root (nthrt)
+# of a number. Do not change the function interface
+
+# Note:
+# your code should be able to check  if an element in S is negative,
+# then the corresponding element in n must be an odd integer.
+
+def nthrt(S,n,x0=1.,tol=1e-5,debug=False):
+    #Input:
+    #       S: a real number
+    #       n:  nth root 
+    #      x0: initial guess. We have provided a default value, so it's optional
+    #      tol: tolerance. Default values is provided as tol=1e-5
+    #debug: a True/False flag to indicate if to turn on the debugging mode
+
+    
